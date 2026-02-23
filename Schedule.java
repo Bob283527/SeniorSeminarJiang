abf3d8340e537e0ce2a4958c18b0dfcaac47e539
Bob283527 <jonjiang27@7hills.org>
2026-02-23 09:32:58 -0500

    I decided to do a popularity ranking of the courses to see which would get to run a second time and then decided to place students based on who was the fastest, although you could argue that a longer time simply meant they were thinking longer I decided time basde would be easiest and quickest solution I am not trying to cater towards the students feelings rather I am trying to do this based on efficiency and what I think would be easiest

---
+++ Schedule.java
@@ -0,0 +1,28 @@
+public class Schedule {
+	
+	public Schedule() {
+		/*should i use the organized data from my ranker.java
+		 * to place the students who submitted first until each course in the first session is filled up
+		 * if the course is filed but the student put it as their first chocie then we look towards their second choice
+		 * we keep going through their choices until  one of the courses have a free spot
+		 * if all 5 of their choices are filled then put them into the course with the least amt of people
+		 * we put the students that got their first chocie into a temp var and out them at the bottom of the array 
+		 * then for second sessiosn we start where we left off so now the students who didnt get their first chocie has a chance to get it now
+		 * and those who got their first chocie will either get their second or third or fourth or fifth
+		 * whenever a student gets one of their 5 choices replace the choice used with a 0
+		 * so now when we start placing students inbto the second session we know to not use the choice that has a 0 
+		 * keep repeating until all 5 sessions and their courses are filled
+		 * 
+		 * do a course popularity ranker to know whch courses should get a second session
+		 * look at the numbers in choice 1 those are worth 5 points tpwards the course counter
+		 * choice 2 is 4 points, choice 3 is 3 points etc...
+		 * the most popular courses get a second time to run
+		 * 
+		 * so any student that did not get their number one choice in the popular course has a chance to get it now based on time
+		 * 
+		 * theres also 2 slots for each course
+		 */
+		 
+	}
+}
+
